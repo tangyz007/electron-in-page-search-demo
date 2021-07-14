47c6005e6de1395c2bd87ae8b1c7a111f8653115
Tang <tangyunzhi916@gmail.com>
2021-07-14 02:52:02 -0700

    search function with electron prompt

---
+++ find.js
@@ -0,0 +1,65 @@
+const { remote, ipcRenderer } = require('electron')
+// var text = ''
+// const { FindInPage } = require('../src/index.js')
+
+
+// let findInPage = new FindInPage(remote.getCurrentWebContents(), {
+//   preload: true,
+//   offsetTop: 6,
+//   offsetRight: 10
+// })
+
+// let findInPage = new FindInPage(remote.getCurrentWebContents(), {
+//   boxBgColor: '#333',
+//   boxShadowColor: '#000',
+//   inputColor: '#aaa',
+//   inputBgColor: '#222',
+//   inputFocusColor: '#555',
+//   textColor: '#aaa',
+//   textHoverBgColor: '#555',
+//   caseSelectedColor: '#555',
+//   offsetTop: 8,
+//   offsetRight: 12
+// })
+
+// ipcRenderer.on('on-find', (e, args) => {
+//   findInPage.openFindWindow()
+// })
+// function getData(){
+//     console.log('Called!!!');
+// }
+
+// ipc.once('actionReply', function(event, response){
+    //     processResponse(response);
+    // })
+    // ipc.send('asynchronous-message', text);
+    // // console.log(text)
+    // ipc.on('asynchronous-reply', function (event, arg) {
+    //     const message = `Asynchronous message reply: ${arg}`
+    //     document.getElementById('async-reply').innerHTML = message
+    //   })
+var search_button = document.getElementById('btn_search')
+search_button.onclick = function () {
+    var message = document.getElementById('message').value;
+    console.log(message)
+    // 使用 ipcRenderer.send 向主进程发送消息。
+    ipcRenderer.send('asynchronous-message', message);
+  }
+  // 监听主进程返回的消息
+  ipcRenderer.on('asynchronous-reply', function (event, arg) {
+    alert(arg);
+  });
+// search_button.addEventListener('click', () => {
+//     text = document.getElementById("myText").value = "Johnny Bravo";
+    
+// })
+ipcRenderer.on('on-find', () => {
+    console.log("success")
+    // window.find("el")
+//     const modalbox = document.getElementById('modalbox');
+//    if (modalbox.style.display === 'block') {
+//      modalbox.style.display = 'none';
+//   } else {
+//     modalbox.style.display = 'block';
+//    }
+  });
--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const electron = require('electron')
-const { app, BrowserWindow, globalShortcut } = electron
+const prompt = require('electron-prompt');
+const { app, BrowserWindow, globalShortcut, ipcMain } = electron
 const path = require('path')
 let win
+let child
 const winURL = 'file://' + path.normalize(`${__dirname}/index.html`)
+const searchURL = 'file://' + path.normalize(`${__dirname}/search.html`)
+// const searchBox = (
+//   <div
+//     id="modalbox"
+//     style={{ display: 'none', position: 'fixed', zIndex: 1 }}
+//   ><input type="text" onChange={Calls.searchPage} />
+//   </div>);
 // const winURL = '../index.html'
+// const search_win = new BrowserWindow({ width: 200, height: 100, frame: false })
 function createWindow () {   
   win = new BrowserWindow({ 
     width: 1280,
@@ -18,11 +28,60 @@ function createWindow () {
   win.on('closed', () => {
     win = null
   })
+  
+  // 监听渲染进程发送的消息
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg)
+  // const reply = arg.split('').reverse().join('');
+  // console.log('reply: ', reply);
+  // 发送消息到主进程
+  event.sender.send('asynchronous-reply', reply);
+});
 
-  win.on('focus', () => {
+  win.on('focus', (event) => {
     globalShortcut.register('CommandOrControl+F', function () {
       if (win && win.webContents) {
-        win.webContents.send('on-find', '')
+      //   prompt({
+      //     title: 'search text',
+      //     label: 'Search:',
+      //     inputAttrs: {
+      //         type: 'text'
+      //     },
+      //     type: 'input',
+      //     parentBrowserWindow: win
+      // })
+      // .then((r) => {
+      //     if(r === null) {
+      //         console.log('user cancelled');
+      //     } else {
+      //         console.log('result', r);
+      //         win.webContents.findInPage(r)
+      //         // event.preventDefault()
+      //         // win.webContents.findInPage(r)
+      //     }
+      // })
+      // .catch(console.error);
+
+        child = new BrowserWindow({ width: 200,
+          height: 200, parent: win, titleBarStyle: 'hiddenInset'})
+          child.loadURL(searchURL)
+          child.on('closed', () => {
+            child = null
+          })
+          child.webContents.on('did-finish-load', ()=>{
+            let code = `var search_button = document.getElementById('btn_search')
+            search_button.addEventListener('click', () => {
+                text = document.getElementById("myText").value = "Johnny Bravo";
+                console.log(text);
+            })`;
+            child.webContents.executeJavaScript()
+          })
+          // child.webContents.send('on-find', '')
+        // child.show()
+        // win.webContents.findInPage("el")
+        // dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] })
+        // search_win.show()
+        // win.webContents.send('on-find', '')
       }
     })
   })
